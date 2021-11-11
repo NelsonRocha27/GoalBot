f6815d53a89dd688bb0ba95df160bce6d536c31a
Nelson <nelsonrocha2727@gmail.com>
2021-11-11 00:15:18 +0000

    Basic bot Token is being passed as enviroment variable '.env'

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 import discord
+import os
+from dotenv import load_dotenv
+
+client = discord.Client()
+load_dotenv()
+
+@client.event
+async def on_ready():
+    print('Bot is now logged in as {0.user}'
+          .format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.getenv('TOKEN'))
