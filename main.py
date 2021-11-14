464d80e91757e6f7bb23b9ec718673a99633faf9
Nelson <nelsonrocha2727@gmail.com>
2021-11-14 02:04:31 +0000

    Adds support to MongoDB Sets prefix for bot Adds a command to add a team to a mongoDB

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import discord
 import os
+import pymongo
+from pymongo import MongoClient
 from dotenv import load_dotenv
+from discord.ext import commands
 
-client = discord.Client()
+# client = discord.Client()
+client = commands.Bot(command_prefix='.')
+
 load_dotenv()
+
+cluster = MongoClient(os.getenv('DBURL'))
+db = cluster["GoalBotDB"]
+collection = db["Teams"]
+
 
 @client.event
 async def on_ready():
     print('Bot is now logged in as {0.user}'
           .format(client))
 
+
+"""
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -17,5 +29,26 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+"""
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+@client.command(aliases=['add'])
+async def add_team(ctx, team):
+    guildIDQuery = {"_id": ctx.guild.id}
+    if collection.count_documents(guildIDQuery) == 0:
+        post = {"_id": ctx.guild.id, "team": [team]}
+        collection.insert_one(post)
+    else:
+        guild = collection.find(guildIDQuery)
+        for result in guild:
+            listOfTeams = result["team"]
+        listOfTeams.append(team)
+        collection.update_one({"_id": ctx.guild.id}, {"$set": {"team": listOfTeams}})
+
 
 client.run(os.getenv('TOKEN'))
